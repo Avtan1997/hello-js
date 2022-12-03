c21c86fdc1b14e1bcd773707ce2078308c09adc4
Avtandil Tursunbekov <nice.avtan@gmail.com>
2022-12-03 14:43:56 +0600

    add Tasks

--- freudianTranlator.js
+++ freudianTranlator.js
@@ -1,46 +1,46 @@
 /** (1 задача) Заменяет каждое слова данное в consol.log за слово Выходной.*/
-// const toFreud = s => !s ? '' : s.split(' ').map(el => 'Выходной').join(' ');
-// console.log(toFreud('Учеба я хочу отдохнуть от тебя :)'))
+const toFreud = s => !s ? '' : s.split(' ').map(el => 'Выходной').join(' ');
+console.log(toFreud('Учеба я хочу отдохнуть от тебя :)'))
 
 /** (2 задача) https://www.codewars.com/kata/575fa9afee048b293e000287/javascript   */
 
-// function howMuchWater(water, load, clothes){
-//   if(clothes > load * 2) return 'Слишком много одежды';
-//   else if (load > clothes) return 'Не достаточно одежды';
-//   else return +(water * 1.1 ** clothes - load).toFixed(2)
-// }
-// console.log(howMuchWater(5, 5, 4));
+function howMuchWater(water, load, clothes){
+  if(clothes > load * 2) return 'Слишком много одежды';
+  else if (load > clothes) return 'Не достаточно одежды';
+  else return +(water * 1.1 ** clothes - load).toFixed(2)
+}
+console.log(howMuchWater(5, 5, 4));
 
 /** (3 задача) Простая функция которая определяет множественное число или нет
  * https://www.codewars.com/kata/52ceafd1f235ce81aa00073a/javascript */
 
-// function plural(n){
-//   if(n < 1) return true
-//   else if (n === 1) return  false
-//   else return true
-// }
-// console.log(plural(1));
+function plural(n){
+  if(n < 1) return true
+  else if (n === 1) return  false
+  else return true
+}
+console.log(plural(1));
 
-// Короткая версия функции
-// function plural2(n){
-//   if(n === 1) return false
-//   else return true
-// }
-// console.log(plural2(2))
+Короткая версия функции
+function plural2(n){
+  if(n === 1) return false
+  else return true
+}
+console.log(plural2(2))
 
 /**(4 Задача) Написать функцию котоарая будет проверять если два символа
  * которые к нам пришли находятся в том же самом регистре. Это код у меня не сработал
  * https://www.codewars.com/kata/5dd462a573ee6d0014ce715b/javascript */
 
-// const isNotLetter = s => s === s.toUpperCase() && s === s.toLowerCase()
-// const defineCase = s => s === s.toUpperCase() ? 'U' : 'L'
-//
-// function sameCase(a, b){
-//   if(isNotLetter(a) || isNotLetter(b)) return -1
-//   return defineCase(a) === defineCase(b) ? 1 : 0
-// }
-//
-// console.log(sameCase(1, 2));
+const isNotLetter = s => s === s.toUpperCase() && s === s.toLowerCase()
+const defineCase = s => s === s.toUpperCase() ? 'U' : 'L'
+
+function sameCase(a, b){
+  if(isNotLetter(a) || isNotLetter(b)) return -1
+  return defineCase(a) === defineCase(b) ? 1 : 0
+}
+
+console.log(sameCase(1, 2));
 
 /** (5 задача Создайте несколько функций,
  * которые будут возвращать сумму, разность, модуль,
