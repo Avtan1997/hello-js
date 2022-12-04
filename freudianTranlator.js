c382a61be7472510ec81ae048becc50bf8eb9f97
Avtandil Tursunbekov <nice.avtan@gmail.com>
2022-12-04 12:27:24 +0600

    add Tasks 04.12.2022

--- freudianTranlator.js
+++ freudianTranlator.js
@@ -73,5 +73,5 @@
 // console.log(multiply(1, 2))
 // console.log(divide(2, 1))
 // console.log(mod(1, 2))
-console.log(exponent(1, 2))
-console.log(subt(1, 2))
+// console.log(exponent(1, 2))
+// console.log(subt(1, 2))
