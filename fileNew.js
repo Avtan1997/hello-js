83d3961dc471ba2da982ad49948ff123919f9683
Avtandil Tursunbekov <nice.avtan@gmail.com>
2022-12-07 23:08:55 +0600

    add Tasks 07.12.2022 17:00

--- fileNew.js
+++ fileNew.js
@@ -1,3 +1,49 @@
+// const name = 'Chynara'
+// const city = 'Bishkek'
+// const greeting = `My name is ${name} and i live in the city ${city}`
+//
+// console.log(greeting)
 
-let i = 1, j = ++i;
-console.log(i)
+//=======================
+// function multByFactor(value, multiplier = 1) {
+//   return value * multiplier
+// }
+//
+// console.log(multByFactor(10, 2));
+// console.log(multByFactor(10));
+
+// const multByFactor = (value, multiplier = 1) => value * multiplier
+// console.log(multByFactor(12,3))
+
+
+// if (n === 1)
+//   console.log("Вы получили 1 новое сообщение.”);
+// else
+//   console.log(`Вы получили ${n} новых сообщений.`);
+
+// let len = book.subtitle.length; // ITypeError: undefined не имеет
+
+// let s = { x: 1, у: 1 }.toString(); // s == "[object Object]"
+// console.log(s)
+
+
+// let o = {х : 1, у: 2, z : 3, toString: () => `Hello`};// Три перечислимых собственных свойства
+// o.propertyIsEnumerable("toString")
+// // console.log(Object.keys(o))
+// // console.log(o)
+//   o.propertyIsEnumerable("toString")// => false: не перечислимое
+// for (let p in o) { // Проход в цикле по свойствам
+// // console.log(p);
+// }
+// console.log(o.toString());
+
+const avtan = {
+  _age: 25,
+  get age() {return this._age},
+  set age(value) {this._age = value}
+}
+avtan.age = 65
+console.log(avtan.age)
+
+
+
