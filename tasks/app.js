f02d9e27e26e6cabced5dfec4f54f331c9e170e8
Avtandil Tursunbekov <nice.avtan@gmail.com>
2022-12-28 20:08:43 +0600

    add Tasks 27.12.2022 21:25

--- tasks/app.js
+++ tasks/app.js
@@ -1,5 +1,5 @@
-const http = require('http');
-
+// const http = require('http');
+//
 // const server = http.createServer((req, res) => {
 //   res.writeHead(200, {'Content-Type' : 'text/plain'});
 //   res.end('Hello Avtan!');
@@ -7,3 +7,76 @@ const http = require('http');
 // });
 //
 // server.listen(3000, () => console.log('Server работает'))
+
+// const name = 'Avtandil'
+//
+// function myName() {
+//   function innerName() {
+//     console.log(name)
+//   }
+//   innerName()
+// }
+//
+// myName()
+
+// const myFunction = (a, b) => {
+//   let c
+//   a = a + 1
+//   c = a + b
+//   return c
+// }
+//
+// console.log(myFunction(1995, 1)
+
+// setTimeout(() => {
+//   console.log('Отложенное сообщение')
+// }, 2000)
+
+// function multByFactor(value, multiplier = 1) {
+//   return value * multiplier
+// }
+//
+// console.log(multByFactor(10, 2));
+// console.log(multByFactor(5));
+//--------------------------------------------------------
+// const newPost = (post, addedAt = Date()) => ({
+//   ...post,
+//   addedAt
+// })
+//
+// const firstPost = {
+//   id: 1,
+//   author: 'Avtandil'
+// }
+//
+// console.table(newPost(firstPost));
+
+//Явное переобразование стрелочных функций
+// const newPost = (post, addedAt = Date()) => {
+//   const myPost = {
+//     ...post,
+//     addedAt
+//   }
+//   return myPost
+// }
+//
+// const firstPost = {
+//   id: 1,
+//   author: 'Avtan'
+// }
+//
+// console.table(newPost(firstPost))
+
+
+// const fnWithError = () => {
+//   throw new Error('Some error')
+// }
+//
+// try {
+//   fnWithError()
+// } catch (error){
+//   console.error(error)
+//   console.log(error.message)
+// }
+//
+// console.log('Continue...')
