cdfd03ded1a6ddaf4a7c4783f983e13b3a509eda
Avtandil Tursunbekov <nice.avtan@gmail.com>
2023-01-23 23:41:32 +0600

    add Tasks 23.01.2023

--- JavaScriptsBaseics/script.js
+++ JavaScriptsBaseics/script.js
@@ -38,4 +38,152 @@
 // colors.push('blue')
 // console.log(colors)
 
+// let x = 'something'
+// console.log(x)
+//
+//  x = 3
+// console.log(x)
+
+//Primitive data types
+
+//String
+// const someText = 'Some text'
+// console.log(typeof someText)
+//
+// //Number
+// const someNumber = 11
+// console.log(typeof someNumber)
+//
+// //Boolean
+// const someBoolean = false
+// console.log(typeof someBoolean)
+//
+// //Null
+// const someNull = null
+// console.log(typeof someNull)
+//
+// //Undefined
+// let someUndefined
+// console.log(typeof someUndefined)
+//
+// //Symbol
+// const someSymbol = Symbol()
+// console.log(typeof someSymbol)
+//
+//
+// //Reference data types
+//
+// //Array
+// const someArray = [1, 2, 3, 4, 5, 6]
+// console.log(typeof someArray)
+//
+// //Object Literal
+// const someObjectLiteral = {
+//   firstProperty: '12',
+//   secondProperty: 'secondProperty'
+// }
+// console.log(typeof someObjectLiteral)
+//
+// //Function
+// const someFunction = new Function()
+// console.log(typeof someFunction)
+//
+// //Date
+// const someDate = new Date()
+// console.log(typeof someDate)
+
+//String()
+// let x = String(1111);
+// x = String(2 + 2)
+// x = String(false)
+// x = String (new Date())
+// x = String(['one', 'two', 'three'])
+// console.log(x);
+// console.log(typeof x)
+// console.log(x.length)
+
+//toString()
+
+// x = (35).toString();
+// x = (true).toString()
+//
+// console.log(x);
+// console.log(typeof x)
+// console.log(x.length)
+
+//Conversion to number
+
+// let y = Number('5.7667674');
+// y = Number(false)
+// y = Number(null)
+// y = Number('text')
+//
+//
+// console.log(y);
+// console.log(typeof y)
+// console.log(y.toPrecision(3))
+
+
+//parseInt()
+
+// const y = ('123')
+//
+// console.log(y)
+// console.log(typeof y)
+// console
+
+// const x = '5'
+// const y = '3'
+// const z = x * y
+//
+// console.log(z)
+// console.log(typeof z)
+
+// const firstNumber = 50
+// const secondNumber = 3
+// let result
+// //Простые математические операции
+// result = firstNumber + secondNumber
+// result = firstNumber - secondNumber
+// result = firstNumber * secondNumber
+// result = firstNumber / secondNumber
+// result = firstNumber % secondNumber
+//
+// //Math object
+// result = Math.PI;
+// result = Math.round(5.8)
+// result = Math.ceil(3.3)
+// result = Math.floor(3.7)
+// result = Math.sqrt(9)
+// result = Math.abs(-4)
+// result = Math.pow(3, 4)
+// result = Math.min(3, 23, 43, 4, 1)
+// result = Math.max(3, 23, 43, 4, 1)
+// result = Math.floor(Math.random() * 100) + 1
+
+
+
+
+// console.log(result)
+
+
+let result
+const firstName = 'Jack'
+const lastName = 'Brown'
+const space = ' '
+const age = 25
+const greeting = 'Hey there!'
+
+
+result = 'Jack'
+result = firstName + space + lastName
+
+result = greeting + space + 'My name is' + space + firstName + ' ' + lastName + space  +  "I'am" + space + age+ ' ' + 'years old.'
+
+
+
+
+console.log(result)
+
+
 
