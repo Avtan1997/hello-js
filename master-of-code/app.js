e6a725265c27a4e6ca62c75d231c4931ce2678c6
Avtandil Tursunbekov <nice.avtan@gmail.com>
2023-02-10 20:05:37 +0600

    add Tasks 07.02.2023

--- master-of-code/app.js
+++ master-of-code/app.js
@@ -1,1 +1,79 @@
-alert('Hello again');
+// y = parseFloat('123.20')
+//
+// console.log(y)
+// console.log(typeof y)
+// console.log(y.toPrecision(5))
+
+// const x = '5'
+// const y = '3'
+// const z = x * y
+//
+// console.log(z)
+// console.log(typeof z)
+
+// const firstNumber = 50;
+// const secondNumber = 3;
+// let result;
+
+//Simple math operations
+// result = firstNumber + secondNumber
+// result = firstNumber - secondNumber
+// result = firstNumber * secondNumber
+// result = firstNumber / secondNumber
+// result = firstNumber % secondNumber
+
+//Math object
+// result = Math.PI;
+// result = Math.round(3.5);
+// result = Math.ceil(3.3);
+// result = Math.floor(3.7);
+// result = Math.sqrt(9);
+// result = Math.abs(-7);
+// result = Math.pow(2,4);
+// result = Math.min(3, 45, 56, 11, -54);
+// result = Math.max(3, 45, 56, 11, -54);
+// result = Math.floor(Math.random() * 100) + 1;
+
+
+
+
+// console.log(result)
+
+//Concatanation
+
+let result;
+const firstName = 'Jack';
+const lastName = 'Brown';
+const space = ' ';
+const age = 25;
+const greeting = 'Hey there!'
+
+result = firstName + space + lastName
+result = greeting + space + 'My name is' + space + firstName + space + lastName + space + 'i am' + space + age + space +
+  'years old!'
+
+//Escaping
+result = `I'am 25 years old`;
+result = "I like \"Star Wars\" movie"
+
+//Properties and methods
+result = 'Hello! '.length
+result = firstName.length
+
+result = firstName.concat(' ',lastName);
+result = greeting.concat(space, 'My name is ', firstName, space, lastName )
+result = result.toUpperCase()
+result = result.toLowerCase()
+
+result = firstName[0]
+result = 'Hello'.indexOf('a')
+result = 'Hello'.lastIndexOf('lo')
+result = firstName.indexOf('x')
+result = firstName.charAt(1)
+
+const longString = 'Hi, I\'m a long string'
+
+result = longString.charAt(longString.length -1)
+
+
+console.log(result)
